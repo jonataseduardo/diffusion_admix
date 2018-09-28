c3394a8b1bf60154ba1ed98429e0ac4ff89399e9
Jonatas Cesar <jonataseduardo@gmail.com>
2018-09-28 11:10:19 -0500

    cross moments seams to be ok

--- scripts/simple_admix.py
+++ scripts/simple_admix.py
@@ -29,7 +29,7 @@ def cross_moments(fs, k):
     
     ktilde = np.tile(k, np.append(ns + 1, 1))
 
-    x = 1. * counts_per_pop / ns
+    x = 1. * counts_per_pop / ns 
 
     x_k = numpy.power(x, ktilde)
     
@@ -43,6 +43,8 @@ def cross_moments(fs, k):
     mu_k = x_k * fstilde
 
     return mu_k.sum()
+
+fs*numpy.arange(fs.sample_sizes + 1) /fs.sample_sizes
 
 def OutOfAfrica_stepwise((nuAf, nuB, nuEu0, nuEu, nuAs0, nuAs, 
                            mAfB, mAfEu, mAfAs, mEuAs, TAf, TB, TEuAs), 
@@ -70,48 +72,48 @@ def OutOfAfrica_stepwise((nuAf, nuB, nuEu0, nuEu, nuAs0, nuAs,
     
     dt = dtAf 
     time = 0
-    key_iter = 0 
+    step = 0 
     split_1 = split_2 = True
     while(time <= 1.001 * Ttotal ):
         time += dt
 
         if(time <= 1.001 * TAf):
-            print('before split' , time )
+            print('before split' , step, time )
             #integrate for time TAf (with constant population)
             fs.integrate([nuAf], dt, dt)
-            time_sfs[key_iter] = [time, fs]
-            key_iter += 1
+            time_sfs[step] = [time, fs]
+            step += 1
 
         if(time > TAf and time <= 1.001 * (TAf + TB)):
             if(split_1):
-                print('first split' , time )
+                print('first split' , step, time )
                 split_1 = False
                 dt = dtB
                 fs = moments.Manips.split_1D_to_2D(fs, n1, n2+n3)
-                time_sfs[key_iter] = [time, fs]
-                key_iter += 1
+                time_sfs[step] = [time, fs]
+                step += 1
                 mig1=numpy.array([[0, mAfB],[mAfB, 0]])
 
             print('after first split' , time )
             fs.integrate([nuAf, nuB], dt, 0.5 * dt, m=mig1)
-            time_sfs[key_iter] = [time, fs]
-            key_iter += 1
+            time_sfs[step] = [time, fs]
+            step += 1
 
         if(time > TAf + TB and time <= 1.001 * (TAf + TB + TEuAs)):
             if(split_2):
-                print('sencond split' , time )
+                print('sencond split' , step, time )
                 split_2 = False
                 dt = dtEuAs
                 fs = moments.Manips.split_2D_to_3D_2(fs, n2, n3)
-                time_sfs[key_iter] = [time, fs]
-                key_iter += 1
+                time_sfs[step] = [time, fs]
+                step += 1
                 # migration rates matrix
                 mig2=numpy.array([[0, mAfEu, mAfAs],
                                   [mAfEu, 0, mEuAs],
                                   [mAfAs, mEuAs, 0]])
                 nuEu0t, nuAs0t = nuEu0, nuAs0
 
-            print('after sencond split' , time )
+            print('after sencond split' , step, time )
 
             #define functions for population sizes
             nuEu_func = lambda t: nuEu0t*(nuEu/nuEu0t)**(t/TEuAs)
@@ -122,18 +124,12 @@ def OutOfAfrica_stepwise((nuAf, nuB, nuEu0, nuEu, nuAs0, nuAs,
             mig2=numpy.array([[0, mAfEu, mAfAs],[mAfEu, 0, mEuAs],[mAfAs, mEuAs, 0]])
             fs.integrate(nu2, dt, 0.5 * dt, m=mig2)
 
-            time_sfs[key_iter] = [time, fs]
+            time_sfs[step] = [time, fs]
             nuEu0t = nuEu0t*(nuEu/nuEu0t)**(dt/TEuAs)
             nuAs0t = nuAs0t*(nuAs/nuAs0t)**(dt/TEuAs)
-            key_iter += 1
+            step += 1
                                 
     return time_sfs
-
-
-#sts = moments.LinearSystem_1D.steady_state_1D(9, 1)
-#fs = moments.Spectrum(sts)
-#fs = moments.Manips.split_1D_to_2D(fs, 3, 6)
-#fs = moments.Manips.split_2D_to_3D_2(fs, 3, 3)
 
 params = np.array([2.10065897, 0.25066579, 0.22247642, 3.05297944,
                    0.09022469, 5.82773903, 3.79104318, 0.25730946,
@@ -143,6 +139,17 @@ params = np.array([2.10065897, 0.25066579, 0.22247642, 3.05297944,
 
 (nuAf, nuB, nuEu0, nuEu, nuAs0, nuAs,mAfB, mAfEu, mAfAs, mEuAs, TAf, TB, TEuAs) = params
 fs_t = OutOfAfrica_stepwise(params, (4, 4, 4), 20)
-fs.keys()
+
+(5. / (5. - 1.)) * 2 * (cross_moments(fs_t[1][1], 1) - cross_moments(fs_t[1][1], 2))
+fs_t[1][1].pi()
 
 fs_admix = moments.Manips.admix_into_new(fs, 0, 1, 2, 0.5)
+
+sts = moments.LinearSystem_1D.steady_state_1D(9, 1)
+fs = moments.Spectrum(sts)
+n = fs.sample_sizes
+(n / (n - 1.)) * 2. * (cross_moments(fs, 1) - cross_moments(fs, 2))
+fs.pi()
+
+#fs = moments.Manips.split_1D_to_2D(fs, 3, 6)
+#fs = moments.Manips.split_2D_to_3D_2(fs, 3, 3)
