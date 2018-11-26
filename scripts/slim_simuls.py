44e115744029ddd7f2d0511869bb17229ca98627
Jonatas Cesar <jonataseduardo@gmail.com>
2018-11-26 17:40:26 -0600

    adding jouganous 2017 model with admixture pop in the end

--- scripts/slim_simuls.py
+++ scripts/slim_simuls.py
@@ -169,6 +169,62 @@ def out_of_africa(n1 = 216, n2 = 198, n3 = 206, **kwargs):
         """
     return demographics.format(n1 = n1, n2 = n2, n3 = n3)
 
+def afr_eur_admix():
+    demographics = """
+        // Create the ancestral African population
+        1 {{ sim.addSubpop("p1", 11273); }}
+
+        // Expand the African population to 23721
+        // This occurs 312000 years (10758) generations ago
+        47242 {{ p1.setSubpopulationSize(23721); }}
+
+        // Split non-Africans from Africans and set up migration between them
+        // This occurs 125 kyears (4310 generations) ago
+        53690 {{
+                sim.addSubpopSplit("p2", 3104, p1);
+                p1.setMigrationRates(c(p2), c(15.8e-5));
+                p2.setMigrationRates(c(p1), c(15.8e-5));
+        }}
+
+        // European subpopulation second bottleneck 
+        // This occurs 42.3 kyears (1459 generations) ago
+        56541 {{
+                sim.addSubpopSplit("p3", 554, p2);
+                p2.setSubpopulationSize(2271);  // reduce European size
+
+                // Set migration rates for the rest of the simulation
+                p1.setMigrationRates(c(p2), c(1.1e-5));
+                p2.setMigrationRates(c(p1), c(1.1e-5));
+        }}
+
+        // Set up exponential growth in Europe and East Asia
+        // Where N(0) is the base subpopulation size and t = gen - 56541:
+        //    N(Europe) should be int(round(N(0) * e^(0.00196*t)))
+        56541:58000 {{
+                t = sim.generation - 56541;
+                p2_size = round(2271 * exp(0.00196 * t));
+                p2.setSubpopulationSize(asInteger(p2_size));
+        }}
+
+        {58001}{{sim.addSubpop("p3", {n});
+              p3.setMigrationRates(c(p1,p2), c(0.25, 0.75));
+              sim.addSubpop("p4", {n});
+              p4.setMigrationRates(c(p1,p2), c(0.5, 0.5));
+              sim.addSubpop("p5", {n});
+              p5.setMigrationRates(c(p1,p2), c(0.75, 0.25));
+              }}
+
+        // Generation 58001 is the present. Output and terminate.
+        58001 late() {{
+                p1.outputSample({n}); 
+                p2.outputSample({n}); 
+                p3.outputSample({n}); 
+                p4.outputSample({n}); 
+                p5.outputSample({n}); 
+        }}
+        """
+    return demographics.format(n = 1000)
+
 def balick_split_model(N0 = 1000, N1 = 100, 
                        Tburn = 100, Tbn = 100, Tpost_bn = 100, 
                        **kwargs): 
@@ -178,11 +234,11 @@ def balick_split_model(N0 = 1000, N1 = 100,
         {T1}{{sim.addSubpopSplit("p2", {N1}, p1); }}
         {T2}{{p2.setSubpopulationSize({N0}); }}
         {T3}{{sim.addSubpop("p3", {N0});
-               p3.setMigrationRates(c(p1,p2), c(0.25, 0.75));
-               sim.addSubpop("p4", {N0});
-               p4.setMigrationRates(c(p1,p2), c(0.5, 0.5));
-               sim.addSubpop("p5", {N0});
-               p5.setMigrationRates(c(p1,p2), c(0.75, 0.25));
+              p3.setMigrationRates(c(p1,p2), c(0.25, 0.75));
+              sim.addSubpop("p4", {N0});
+              p4.setMigrationRates(c(p1,p2), c(0.5, 0.5));
+              sim.addSubpop("p5", {N0});
+              p5.setMigrationRates(c(p1,p2), c(0.75, 0.25));
               }}
 
         {T3} late() {{
@@ -239,7 +295,7 @@ def run_slim(slim_cmd, output = None):
 
 @click.option("-mu", "--mutation-rate", 
               help = "Mutation rate per pair base",
-              default=1e-7, 
+              default=1.44e-8, 
              show_default=True,
               required=True, 
               type=float
@@ -298,38 +354,42 @@ def run_slim(slim_cmd, output = None):
               default="single_pop", 
               show_default=True,
               required=False, 
-              type=click.Choice(["single_pop", "balick2017", "gravel2013"])
+              type=click.Choice(["single_pop", 
+                                 "balick2015", 
+                                 "gravel2013", 
+                                 "jouganous2017"]
+                                )
               )
 @click.option("-N0", "--pop0-size", 
-              help = "Ancestral pop size for single_pop or balick2017 models",
+              help = "Ancestral pop size for single_pop or balick2015 models",
               default=int(1e3), 
               show_default=True,
               required=True, 
               type=int
               )
 @click.option("-N1", "--pop1-size", 
-              help = "bottleneck pop size balick2017 model",
+              help = "bottleneck pop size balick2015 model",
               default=int(1e2), 
               show_default=True,
               required=True, 
               type=int
               )
 @click.option("-t0", "--time-burn", 
-              help = "burn time for single_pop or balick2017 models",
+              help = "burn time for single_pop or balick2015 models",
               default=int(1e3), 
               show_default=True,
               required=True, 
               type=int
               )
 @click.option("-tb", "--time-bottleneck", 
-              help = "bottleneck time in the balick2017 model",
+              help = "bottleneck time in the balick2015 model",
               default=int(1e3), 
               show_default=True,
               required=True, 
               type=int
               )
 @click.option("-tp", "--time-postbottleneck", 
-              help = "post bottleneck time the balick2017 model", 
+              help = "post bottleneck time the balick2015 model", 
               default=int(1e3), 
               show_default=True,
               required=True, 
@@ -388,6 +448,8 @@ def main(mutation_model,
 
     if demographic_model == "gravel2013":
         demography = out_of_africa()
+    elif demographic_model == "jouganous2017":
+        demography = out_of_africa()
     elif demographic_model == "single_pop":
         demography = single_pop(N0 = pop0_size,
                                 Tburn = time_burn)
@@ -414,5 +476,4 @@ if __name__ == "__main__":
     #demography = balick_split_model(N0 = 100, N1 = 10, Tburn = 1000, mu = 2)
     #print init + demography
     main()
-
 
