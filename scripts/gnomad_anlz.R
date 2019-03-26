8ba0ad3a38f5b9d7472cc9156610b9192cf112c2
jonataseduardo <jonataseduardo@gmail.com>
2019-03-26 16:02:20 -0300

    the sfs projection is working when testing with equlibrium 2d sfs, but it loses a lot of variants in the real data

--- scripts/gnomad_anlz.R
+++ scripts/gnomad_anlz.R
@@ -1,18 +1,41 @@
 library(data.table)
 library(parallel)
+install.packages("normalize")
 
 ## Functions 
+
+rm_fixed_alleles <-
+  function(ac_an_dt, ac, an){
+   return(ac_an_dt[!Reduce(`&`, Map(`==`, mget(ac), 0))
+                  ][!Reduce(`&`, Map(`==`, mget(ac), mget(an)))])
+  }
 
 filter_gad <-
   function(gad,
            filter_pass = TRUE, 
-					 only_nonsynonymous = TRUE, 
-					 remove_fixed = TRUE,
-					 remove_AA = TRUE,
-					 remove_mult_al = TRUE,
-					 an_th = 0.7){
+           only_nonsynonymous = TRUE, 
+           remove_fixed = TRUE,
+           remove_AA = TRUE,
+           remove_mult_al = TRUE,
+           score_to_numeric = TRUE, 
+           an_th = 0.7){
 
-    setnames(gad, "GERP++_RS", "GERP_RS_score")
+    if(filter_pass)
+      gad <- gad[FILTER == "PASS"]
+
+    if(only_nonsynonymous)
+      gad <- gad[ExonicFunc.refGene == "nonsynonymous SNV"]
+
+    #Remove alleles with ancestral information
+    if(remove_AA)
+      gad <- gad[AA == ""] 
+
+
+    #Changing some names in the annotated tables
+    phy <- grep("(vertebrate$)|(mammalian$)|(logOdds$)", 
+                names(gad), value = TRUE)
+    phy_scores <- paste0(phy, "_score")
+    setnames(gad, c("GERP++_RS", phy), c("GERP_RS_score", phy_scores))
 
     snv_info <-
       c("Chr", "Start", "Ref","Alt","AA", "rsid")
@@ -30,41 +53,30 @@ filter_gad <-
 
     gad <- gad[, ..cols_info]
 
-    if(filter_pass)
-			gad <- gad[FILTER == "PASS"]
-
-		if(only_nonsynonymous)
-			gad <- gad[ExonicFunc.refGene == "nonsynonymous SNV"]
-
-		#Remove alleles with ancestral information
-		if(remove_AA)
-			gad <- gad[AA == ""] 
-
     ac <- grep("AC_", pop_info, value = TRUE)
     an <- grep("AN_", pop_info, value = TRUE)
-    
+
     #Remove fixed variants
-	  if(remove_fixed){	
-			gad <- 
-				gad[!Reduce(`&`, Map(`==`, mget(ac), 0))
-						][!Reduce(`&`, Map(`==`, mget(ac), mget(an)))]
-		}
+    if(remove_fixed) 
+      gad <- rm_fixed_alleles(gad, ac, an)
 
     #remove multiallelic snps
-		if(remove_mult_al){
-			gad[, N := .N, by = .(Chr, Start, Ref)]
-			gad <- gad[N == 1]
-			gad[, N := NULL]
-		}
+    if(remove_mult_al){
+      gad[, N := .N, by = .(Chr, Start, Ref)]
+      gad <- gad[N == 1]
+      gad[, N := NULL]
+    }
 
-		#remove snps secreend in less an_th proportion of samples
-		if(an_th < 0.99){
-			max_an <- gad[, lapply(.SD, max), .SDcols = an]
-			max_an <- an_th * max_an
-			gad <- gad[Reduce(`&`, Map(`>=`, mget(an), max_an[, mget(an)]))]
-		}
+    #remove snps secreend in less an_th proportion of samples
+    if(an_th < 0.99){
+      max_an <- gad[, lapply(.SD, max), .SDcols = an]
+      max_an <- an_th * max_an
+      gad <- gad[Reduce(`&`, Map(`>=`, mget(an), max_an[, mget(an)]))]
+    }
 
-    gad[, (del_info) := lapply(.SD, as.numeric), .SDcols = del_info]
+    if(score_to_numeric)
+      gad[, (del_info) := lapply(.SD, as.numeric), .SDcols = del_info]
+
     return(gad)
   }
 
@@ -83,6 +95,36 @@ make_delrank <-
 
     gad[, (del_rank) := lapply(.SD, frank, na.last = FALSE), .SDcols = del_info]
     gad[, (del_rank) := 1.0 - .SD / .N , .SDcols = del_rank]
+
+    return(gad)
+  }
+
+normalize <-
+  function(x){
+    x_min <- min(x, na.rm = TRUE)
+    x_max <- max(x, na.rm = TRUE)
+    if((x_min == 0) & (x_max == 1)){
+      return(x)
+    }else{
+      x_wd <- x_max - x_min
+      return((x - x_min)/x_wd)
+    }
+  }
+
+normalize_del_score <- 
+  function(gad,
+           inplace = TRUE){
+
+    if(!inplace)
+      gad <- copy(gad)
+
+    del_info <-
+      grep("(_score$)|([_-]raw$)", names(gad), value = TRUE)
+
+    del_norm <- 
+      gsub("^(.+)(_score$)|([_-]raw$)", "\\1_norm", del_info)
+
+    gad[, (del_norm) := lapply(.SD, normalize), .SDcols = del_info]
 
     return(gad)
   }
@@ -174,31 +216,38 @@ af_from_sfs_1d <-
 af_from_sfs <- 
   function(sfs){
     ac <- 
-      grep("^AC_[a-z]{3}$", names(gad), value = TRUE)
+      grep("^AC_[a-z]{3}$", names(sfs), value = TRUE)
 
     an <- 
-      grep("^AN_[a-z]{3}$", names(gad), value = TRUE)
+      grep("^AN_[a-z]{3}$", names(sfs), value = TRUE)
 
     af <- 
-      grep("^AF_[a-z]{3}$", names(gad), value = TRUE)
+      gsub("(^AN)(_[a-z]{3}$)", "AF\\2", an)
 
     sfs[, (af) := Map(`/`, mget(ac), mget(an))] 
     return(sfs[rep(1:.N, num_sites)])
   }
 
 project_sfs_1d <-
-  function(sfs_dt, AN_new, groups = "all_groups"){
+  function(sfs_dt, AN_new, groups = "all_groups", folded = TRUE){
     
     if(groups == "all_groups")
       groups <- setdiff(names(sfs_dt), c("AN", "AC", "num_sites"))
 
+    if(folded){
+      AN_new_fold <- AN_new / 2
+    }else{
+      AN_new_fold <- AN_new
+    }
+
     sfs_project <- 
-      sfs_dt[, .(AC_new = 1:min((AN_new) -1, AC), 
-                 AN_new = (AN_new)), 
-              by = c(groups, c("AN", "AC", "num_sites"))
-            ][, proj_i := dhyper(AC_new, AN_new, AN - AN_new, AC)
-            ][, .(num_sites_new = sum(num_sites * proj_i)), 
-              keyby = c(groups, c("AN_new", "AC_new"))]
+      sfs_dt[, .(AC_new = 0:min((AN_new), AC), 
+                 AN_new = (AN_new),
+                 num_sites = num_sites), 
+              by = c(groups, c("AN", "AC"))
+             ][, proj_i := dhyper(AC_new, AN_new, AN - AN_new, AC)
+             ][, .(num_sites_new = round(sum(num_sites * proj_i), digits = 0)), 
+               keyby = c(groups, c("AN_new", "AC_new"))]
 
     setnames(sfs_project, 
              paste0(c("AN", "AC", "num_sites"), "_new"),
@@ -210,18 +259,24 @@ project_sfs_1d <-
   }
 
 project_onepop_sfs  <- 
-  function(sfs_dt, pop, AN_new){
+  function(sfs_dt, pop, AN_new, folded = TRUE){
 
     old_names <- names(sfs_dt)
     setnames(sfs_dt, paste0(c("AN_", "AC_"), pop), c("AN", "AC"))
 
     sfs_project <- 
-      project_sfs_1d(sfs_dt, AN_new)
+      project_sfs_1d(sfs_dt, AN_new, folded = folded)
 
     setnames(sfs_dt, c("AN", "AC"), paste0(c("AN_", "AC_"), pop))
     setnames(sfs_project, c("AN", "AC"), paste0(c("AN_", "AC_"), pop))
 
     setcolorder(sfs_project, old_names)
+
+    ac <- grep("AC_", names(sfs_project), value = TRUE)
+    an <- grep("AN_", names(sfs_project), value = TRUE)
+    sfs_project <-
+      rm_fixed_alleles(sfs_project, ac, an)  
+
     return(sfs_project)
   }
 
@@ -232,7 +287,7 @@ gad_files <-
        list.files("../gnomad_data", full.names = TRUE), 
        value = TRUE)
 
-# gad1 <- fread("../gnomad_data/gnomad.exomes.r2.1.sites.chr1.annotated.csv")
+#gad1 <- fread("../gnomad_data/gnomad.exomes.r2.1.sites.chr1.annotated.csv")
 system.time(
 gad <- 
   rbindlist(mclapply(gad_files, 
@@ -245,8 +300,15 @@ setkey(gad, Chr, Start, Ref)
 system.time(make_delrank(gad))
 system.time(make_delquantile(gad))
 system.time(rescale_ac(gad))
+system.time(normalize_del_score(gad))
 
-gad[,unique(REVEL_quantile)]
+norm_scores <- 
+  grep("_norm", names(gad), value = TRUE) 
+
+score_names <- gsub("(^.+)(_norm$)", "\\1", norm_scores)
+
+gad[, quantile(GERP_RS_norm, seq(0,1, 0.2), na.rm = TRUE)] 
+
 
 
 scols <- 
@@ -254,20 +316,76 @@ scols <-
 			"GERP_RS_score", "GERP_RS_rank", "GERP_RS_quantile",
       "AC_afr", "AN_afr", "AC_amr", "AN_amr", "AC_nfe", "AN_nfe")
 
-pop_list <- 
-      c("afr", "amr", "nfe")
+pop_list <- c("afr", "amr", "nfe")
 
-score <- "GERP_RS"
-
-sfs <- sfs_from_ac(gad, pop_list, score)
-setkeyv(sfs_small, key(sfs))
-system.time(sfs_small <- project_onepop_sfs(sfs, "afr", 16256))
-all.equal(sfs_small, sfs)
+score <- "Eigen"
 
 
-sfs_1d <- sfs_1d_from_ac(gad, pop_list[1], score)
-system.time(sfs_small_1d <- project_sfs_1d(sfs_1d, 16256))
-setkeyv(sfs_small_1d, key(sfs_1d))
-all.equal(sfs_small_1d, sfs_1d)
+sfs_afr_full <- sfs_1d_from_ac(gad, pop = 'afr', score = score)
+system.time(sfs_afr_small <- project_sfs_1d(sfs_afr_full, AN_afr, folded = FALSE))
+setkeyv(sfs_afr_small, key(sfs_afr_full))
+all.equal(sfs_afr_small, sfs_afr_full)
+
+
+
+
+ac <- paste0("AC_", pop_list)
+an <- paste0("AN_", pop_list)
+
+
+
+sfs_full <- sfs_from_ac(gad, pop_list, score)
+
+sfs_afr_nfe <- sfs_from_ac(gad, c("afr", "nfe"), score)
+
+
+
+sfs_afr_nfe1 <- sfs_afr_nfe[score == score & quantiles == "[0,0.01]"]
+sfs_small_afr_nfe <- 
+  project_onepop_sfs(sfs_afr_nfe[score == score & quantiles == "[0,0.01]"] , "nfe", AN_afr)
+
+sfs_afr_nfe1[, mean(AC_afr/ AN_afr * num_sites)]
+sfs_small_afr_nfe[, mean(AC_afr/ AN_afr * num_sites)]
+sfs_afr_nfe1[, mean(AC_nfe/ AN_nfe * num_sites)]
+sfs_small_afr_nfe[, mean(AC_nfe/ AN_nfe * num_sites)]
+sfs_small_afr_nfe
+
+
+sfs_full <- rm_fixed_alleles(sfs_full, ac, an)
+sfs_full
+
+sfs_small2 <- copy(sfs_small)
+sfs_small2[, num_sites := round(num_sites)]
+sfs_small2[num_sites > 0]
+sfs_full
+
+AN_afr <- sfs_full[1, AN_afr] 
+sfs_small <- sfs_full[quantiles == "[0,0.01]"]
+pop = "afr"
+system.time(
+for(pop in pop_list[2:3]){
+  sfs_small <- project_onepop_sfs(sfs_small, pop, AN_afr)
+}
+)
+
+sfs_small
+af_small <- af_from_sfs(sfs_small)
+af_small1 <- af_from_sfs(sfs_small1)
+
+sfs_small[, mean(AC_afr/ AN_afr * num_sites)] * 1000
+af_small[, .(mean(AF_afr))] * 1000
+af_small1[, .(mean(AF_afr))] * 1000
+
+round(0.509098, digits = 1)
+
+sfs_small[, .(max(AC_afr), max(AC_amr), max(AC_nfe))]
+sfs_small1[, .(max(AC_afr), max(AC_amr), max(AC_nfe))]
+sfs_small2[, .(max(AC_afr), max(AC_amr), max(AC_nfe))]
+
+sfs_small[,sum(num_sites), AC_afr]
+sfs_small[, .N]
+sfs_small[num_sites == 1, .N]
+
+an
 
 
