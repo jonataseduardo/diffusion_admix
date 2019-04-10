eac00d5b8b32f1c53bb579866ce713e71462033e
jonataseduardo <jonataseduardo@gmail.com>
2019-04-10 15:59:49 -0300

    starting to creating scripts for different analysis, this one is too confuse

--- scripts/gnomad_anlz.R
+++ scripts/gnomad_anlz.R
@@ -1,17 +1,24 @@
 library(data.table)
 library(parallel)
 library(ggplot2)
-
 source("gnomad_ultis.R")
 
 ## Main Code
-
 gad_files <-
   grep("chr[0-9]{1,2}.annotated.csv", 
        list.files("../gnomad_data", full.names = TRUE), 
        value = TRUE)
 
-#gad1 <- fread("../gnomad_data/gnomad.exomes.r2.1.sites.chr1.annotated.csv")
+gad1 <- fread("../gnomad_data/gnomad.exomes.r2.1.sites.chr1.annotated.csv")
+
+gad11 <- filter_gad(gad1, 
+                    filter_pass = TRUE, 
+                    only_nonsynonymous = FALSE, 
+                    remove_fixed = TRUE,
+                    remove_AA = FALSE,
+                    remove_mult_al = TRUE,
+                    score_to_numeric = TRUE)
+
 system.time(
 gad <- 
   rbindlist(mclapply(gad_files, 
@@ -21,12 +28,6 @@ gad <-
 )
 
 setkey(gad, Chr, Start, Ref)
-system.time(make_delrank(gad))
-system.time(rescale_ac(gad))
-system.time(normalize_del_score(gad))
-system.time(make_delquantile(gad, breaks = c(seq(0, 0.5, 0.1), 1)))
-
-system.time(summarise_scores_norm_quantile(gad))
 
 del_info <-
   grep("_quantile$", names(gad), value = TRUE)
@@ -36,6 +37,24 @@ pop_freq <-
 
 pop_list <- 
   sort(gsub("(^AF_)([a-z]{3}$)", "\\2", pop_freq))
+
+AN_afr <- 
+  gad[, max(AN_afr)]
+
+gad_small <- 
+  mc_downsampling(gad, pop_list[2:5], AN_afr)
+
+system.time(rescale_ac(gad))
+system.time(make_delrank(gad))
+system.time(normalize_del_score(gad))
+system.time(make_delquantile(gad, breaks = c(seq(0, 0.5, 0.1), 1)))
+system.time(summarise_scores_norm_quantile(gad))
+
+system.time(rescale_ac(gad_small))
+system.time(make_delrank(gad_small))
+system.time(normalize_del_score(gad_small))
+system.time(make_delquantile(gad_small, breaks = c(seq(0, 0.25, 0.05), 1)))
+system.time(summarise_scores_norm_quantile(gad_small, fname = 'teste_small.png'))
 
 pop_comb <- combn(pop_list[c(1,2,4)], 2)
 pop_comb <- combn(pop_list, 2)
@@ -56,47 +75,85 @@ ttest_data <-
              ac <- paste0("AC_", c(pop1, pop2))
              an <- paste0("AN_", c(pop1, pop2))
              t_dt <- 
-               apply_ttest(rm_fixed_alleles(gad, ac, an),
+               apply_ttest(rm_fixed_alleles(gad_small, ac, an),
                            pop1, pop2, score, inplace = TRUE)
-
              return(t_dt)
            }, mc.cores = length(scores_list)))
          })
   )
 )
 
-setkey(ttest_data1, score, pop1, pop2, quantiles)
+gad1_small <- 
+  mc_downsampling(gad11, 
+                  pop_list[2:5], 
+                  AN_afr)
 
-ttest_data1 <- 
-  copy(ttest_data)
+gad1_small[AA != "", .(mean(AF_afr), mean(AF_nfe), .N), 
+           by = ExonicFunc.refGene]
 
-ttest_data[pop1 == 'afr' & pop2 == 'nfe' & p.value < 0.05][order(quantiles)]
-ttest_data[pop1 == 'afr' & pop2 == 'nfe' & p.value < 0.05][, .N, by = .(score)]
+gad1_small[AA  == "" & AF_afr < 0.05 & AF_nfe < 0.05  , 
+           .(mean(AF_afr), mean(AF_nfe)),
+           by = ExonicFunc.refGene]
 
-ttest_data[p.value < 0.05][pop1 == 'afr']
+gad1_small[, .(mean(AF_afr), mean(AF_nfe)), 
+           by = ExonicFunc.refGene]
 
-ttest_data[pop1 == 'afr' & mean_AF_pop1 < mean_AF_pop2 & p.value < 0.5]
+gad1_small[(Ref == AA | Alt == AA) & 
+           ExonicFunc.refGene == 'nonsynonymous SNV', 
+           t.test(AF_afr, AF_nfe)]
 
-ttest_data[score == 'Polyphen2_HDIV' & p.value < 0.05]
+gad1_small[AA == "" &  AF_afr  < 0.05 & AF_nfe  < 0.05
+           ExonicFunc.refGene == 'nonsynonymous SNV', 
+           t.test(AF_afr, AF_nfe)]
 
-scols <- c(paste0(score, '_quantile'), pop_freq)
+ttest_afr_nfe <- 
+  ttest_data[pop1 == 'afr' & pop2 == 'nfe']
 
-scols <- 
-    c("Chr", "Start", "Ref","Alt", "rsid", 
-			"GERP_RS_score", "GERP_RS_rank", "GERP_RS_quantile",
-      "AC_afr", "AN_afr", "AC_amr", "AN_amr", "AC_nfe", "AN_nfe")
+ttest_afr_nfe[mean_AF_pop1 < mean_AF_pop2]
 
+long_tt <- 
+  melt(ttest_afr_nfe, 
+       measure = patterns(c("var_", "mean_")),
+       variable.name = 'pop',
+       value.name = c("var_AF", "mean_AF")
+       )
 
-score = scores_list[21]
+slist <- scores_list[c(2,22,15,24)]
+slist <- c('VEST3', 'M-CAP', 'MutPred', 'DANN')
 
-sfs_afr_full <- sfs_1d_from_ac(gad, pop = 'afr', score = score)
+long_tt[pop == 1, pop := pop1]
+long_tt[pop == 2, pop := pop2]
+
+{
+ggplot(long_tt[score %in% slist]) + 
+  geom_errorbar(aes(x = quantiles, 
+                    ymax = mean_AF + 1.96 * sqrt(var_AF / num_sites),
+                    ymin = mean_AF - 1.96 * sqrt(var_AF / num_sites),
+                    color = pop), 
+                position = position_dodge()) + 
+  geom_text(data = long_tt[score %in% slist][pop == 'afr'],
+            aes(x = quantiles, 
+                 y = mean_AF + 10.0 * sqrt(var_AF / num_sites), 
+                 label = plabel(p.value),
+                 vjust = 'top'),
+                size = 2.5 
+             ) + 
+  geom_point(aes(x = quantiles, y = mean_AF, color = pop), 
+             position = position_dodge(0.9)) + 
+  labs(y = 'mean(AF)') + 
+  facet_wrap( ~score) + 
+  theme_classic()  
+  ggsave('mean_AF_afr_nfe_small.png')
+}
+
+ttest_afr_nfe[, .N, by = score]
+sfs_afr_full <- 
+  sfs_1d_from_ac(gad, pop = 'afr', score = score)
 
 system.time(
-  sfs_afr_small <- project_sfs_1d(sfs_afr_full, AN_afr, folded = FALSE))
-
-
-setkeyv(sfs_afr_small, key(sfs_afr_full))
-all.equal(sfs_afr_small, sfs_afr_full)
+  sfs_afr_small <- 
+    project_sfs_1d(sfs_afr_full, AN_afr, folded = FALSE)
+)
 
 ac <- paste0("AC_", pop_list)
 an <- paste0("AN_", pop_list)
@@ -128,21 +185,25 @@ sfs_small_afr_nfe <-
   ))
 )
 
-
 conditions <- 
   c('all', 'AC_afr > 0', 'AC_nfe > 0', '(AC_afr > 0) & (AC_nfe > 0)')
+
+sfs_small <- 
+  rbindlist(
+  list(sfs_afr_nfe[quantiles == "[0,0.1]"], 
+       sfs_small_afr_nfe))
 
 system.time(
 mdt <- 
   rbindlist(
   lapply(
-   -1L:4L, 
+    -1L:4L, 
     function(digits){
     rbindlist(
     lapply(
      conditions, 
      function(cond){
-       emv_sfs_afr_nfe(sfs_small_afr_nfe, 
+       emv_sfs_afr_nfe(sfs_small, 
                        conditional = cond, 
                        digits = digits)
      }
@@ -152,4 +213,27 @@ mdt <-
 )
 
 setkey(mdt, score, quantiles, AN_nfe, digits, conditional)
-mdt[digits == -1]
+
+mdt_long <- 
+  melt(mdt, measure = patterns(c("var_", "mean_")),
+       variable.name = 'pop',
+       value.name = c("var_AF", "mean_AF")
+       )
+
+mdt_long[pop == 1, pop := 'afr']
+mdt_long[pop == 2, pop := 'nfe']
+mdt_long[digits == -1, digits := 5] 
+mdt_long[, digits := factor(digits)]
+mdt_long[, conditional := factor(conditional, levels = conditions)]
+
+{
+ggplot(mdt_long) + 
+  geom_line(aes(x = AN_nfe, y = total_num_sites, color = digits, linetype = pop)) + 
+  geom_point(aes(x = AN_nfe, y = total_num_sites, color = digits, shape = pop)) + 
+  theme_classic() + 
+  scale_color_brewer(palette = 'Spectral') + 
+  scale_x_continuous(breaks = seq(AN_afr, AN_nfe, delta_AN)) + 
+  facet_wrap( ~ conditional, scales = 'free_y')
+  ggsave('numsites_project_nfe.png')
+}
+
